0a915f5e789daa9e00857dca9da2e9d4b2602955
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2024-10-10 15:16:10 -0300

    Feat: create project

---
+++ src/routes/suspeitos.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+
+const suspeitosRoutes = Router();
+
+let suspeitos = [
+    {
+        id: 1,
+        nome: "Gustavo Lima",
+        profissao: "Cantor",
+        envolvimento: yes,
+        nivelSuspeita: "baixo"
+    },
+
+        {
+            id: 2,
+            nome: "Lula",
+            profissao: "Presidente",
+            envolvimento: yes,
+            nivelSuspeita: "alto"
+        },
+
+    {
+        id: 3,
+        nome: "Paulo Mauf",
+        profissao: "Politico",
+        envolvimento: no,
+        nivelSuspeita: "médio"
+    }
+];
+
+
+  
+
+    
