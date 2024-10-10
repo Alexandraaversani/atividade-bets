044bab398a5cae21f9ce8bfefe5c1bd5207e666d
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2024-10-10 15:44:34 -0300

    Feat: finalization of requirements

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -28,7 +28,45 @@ let suspeitos = [
     }
 ];
 
+// Rota para listar todos os suspeitos
+suspeitosRoutes.get("/", (req, res) => {
+    return res.status(200).json(suspeitos);
+  });
 
+  // Rota para buscar um suspeitos pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    // Busca um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((perseguido) => perseguido.id == id);
+  
+    // Verifica se o suspeito foi encontrado
+    if (!suspeito) {
+      return res
+        .status(404)
+        .json({ message: `Suspeito com id ${id} não encontrado!` });
+    }
+  
+    return res.status(200).json(suspeito);
+  });
+
+  // Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, profissao, envolvimento, nivelSuspeita} = req.body;
+});
+
+
+ // Remove o suspeito do array de suspeitos
+ suspeitos = suspeitos.filter((suspeito) => suspeito.id != id);
+
+ return res.status(200).json({
+   message: "Suspeito removido com sucesso!",
+   suspeito,
+ });
+
+
+export default suspeitosRoutes;
   
 
     
